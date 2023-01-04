d4308485a48dfd4523c53effdc988a9de430ec6d
Ivan <93490929+Augusha@users.noreply.github.com>
2023-01-04 20:03:21 +0200

    Add files via upload

---
+++ Polymorphism/src/Operation.java
@@ -0,0 +1,18 @@
+class Operation {
+    static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+    static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+    static double Multiply(int a, double b)
+    {
+        return a * b;
+    }
+    static double Multiply(double a, int b)
+    {
+        return a * b;
+    }
+}
